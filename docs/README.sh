7669b654bf1d24a763b170d40a4efa3efc68e9fe
Admin <chris@postfmly.com>
2026-06-01 21:20:17 -0500

    docs: update script

--- docs/README.sh
+++ docs/README.sh
@@ -1,7 +1,5 @@
 #!/usr/bin/env -S bash -e
 
-pushd .. > /dev/null
-
 echo -e "📌 Packages:\n"
 
 _bun=$(bun --version)
@@ -11,13 +9,11 @@ export _bun
 echo -e " • Bun: $_bun"
 
 _coverage=-1
-if [ -f "coverage/lcov.info" ]; then
-  _coverage=$(bun run lcov-total coverage/lcov.info)
+if [ -f "../coverage/lcov.info" ]; then
+  _coverage=$(bun run lcov-total ../coverage/lcov.info)
 fi
 export _coverage
 echo -e "\n☂️  Coverage: $_coverage%"
-
-popd > /dev/null
 
 echo -e "\n🛠️  Creating README.md..."
 
